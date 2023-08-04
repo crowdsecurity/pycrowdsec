ee09888197b6570e2e023501935ba83651a2afb5
Shivam Sandbhor <shivam.sandbhor@gmail.com>
2023-08-04 15:55:09 +0530

    Modernize pycrowdsec
    - Added support for mTLS
    - Add StreamDecisionClient

--- src/pycrowdsec/client.py
+++ src/pycrowdsec/client.py
@@ -1,6 +1,15 @@
 import logging
+import queue
 import threading
-from importlib.metadata import version
+from abc import ABC, abstractmethod
+
+try:
+    from importlib import metadata
+except ImportError:  # for Python<3.8
+    import importlib_metadata as metadata
+
+__version__ = metadata.version("pycrowdsec")
+
 from time import sleep
 
 import requests
@@ -10,12 +19,30 @@ from pycrowdsec.cache import Cache, RedisCache
 logger = logging.getLogger(__name__)
 
 
+def create_session(api_key, insecure_skip_verify, key_path, cert_path, ca_cert_path, user_agent):
+    session = requests.Session()
+    session.verify = not insecure_skip_verify
+    if api_key:
+        session.headers.update(
+            {"X-Api-Key": api_key, "User-Agent": user_agent},
+        )
+    else:
+        if ca_cert_path:
+            session.verify = ca_cert_path
+        session.cert = (cert_path, key_path)
+    return session
+
+
 class QueryClient:
     def __init__(
         self,
-        api_key,
+        api_key="",
         lapi_url="http://localhost:8080/",
-        user_agent=f"python-bouncer/{version('pycrowdsec')}",
+        user_agent=f"python-bouncer/{metadata.version('pycrowdsec')}",
+        insecure_skip_verify=False,
+        key_path="",
+        cert_path="",
+        ca_cert_path="",
     ):
         """
         Parameters
@@ -26,26 +53,39 @@ class QueryClient:
             Base URL of CrowdSec API. Default is http://localhost:8080/ .
         """
 
-        self.api_key = api_key
+        if api_key == "" and key_path == "" and cert_path == "":
+            raise ValueError("You must provide an api_key or a key_path and cert_path")
+
         self.lapi_url = lapi_url
-        self.user_agent = user_agent
+        self.session = create_session(
+            api_key, insecure_skip_verify, key_path, cert_path, ca_cert_path, user_agent
+        )
+
+    def get_decisions_for(self, item):
+        resp = self.session.get(f"{self.lapi_url}v1/decisions?ip={item}")
+        resp.raise_for_status()
+        return resp.json()
 
     def get_action_for(self, item):
-        resp = requests.get(
-            f"{self.lapi_url}v1/decisions?ip={item}", headers={"X-Api-Key": self.api_key}
-        ).json()
-        if resp:
-            return max(resp, key=lambda d: d["id"])["type"]
+        decisions = self.get_decisions_for(item)
+        return max(decisions, key=lambda d: d["id"])["type"]
 
 
-class StreamClient:
+class BaseStreamClient(ABC):
     def __init__(
         self,
         api_key,
         lapi_url="http://localhost:8080/",
         interval=15,
-        user_agent=f"python-bouncer/{version('pycrowdsec')}",
+        user_agent=f"python-bouncer/{__version__}",
         scopes=["ip", "range"],
+        include_scenarios_containing=[],
+        exclude_scenarios_containing=[],
+        only_include_decisions_from=[],
+        insecure_skip_verify=False,
+        key_path="",
+        cert_path="",
+        ca_cert_path="",
         **kwargs,
     ):
         """
@@ -62,10 +102,9 @@ class StreamClient:
         scopes(Optional) : List[str]
             List of decision scopes which shall be fetched. Default is ["ip", "range"]
         """
-        if "redis_connection" in kwargs:
-            self.cache = RedisCache(redis_connection=kwargs["redis_connection"])
-        else:
-            self.cache = Cache()
+
+        if api_key == "" and key_path == "" and cert_path == "":
+            raise ValueError("You must provide an api_key or a key_path and cert_path")
 
         self.api_key = api_key
         self.scopes = scopes
@@ -73,39 +112,70 @@ class StreamClient:
         self.lapi_url = lapi_url
         self.user_agent = user_agent
         self.death_reason = None
+        self.include_scenarios_containing = include_scenarios_containing
+        self.exclude_scenarios_containing = exclude_scenarios_containing
+        self.only_include_decisions_from = only_include_decisions_from
+
+        self.session = create_session(
+            api_key, insecure_skip_verify, key_path, cert_path, ca_cert_path, user_agent
+        )
+        self.__post_init__(**kwargs)
+
+    def cycle(self, first_time):
+        try:
+            resp = self.session.get(
+                url=f"{self.lapi_url}v1/decisions/stream",
+                params={
+                    "startup": first_time,
+                    "scopes": ",".join(self.scopes),
+                    "scenarios_containing": ",".join(self.include_scenarios_containing),
+                    "scenarios_not_containing": ",".join(self.exclude_scenarios_containing),
+                    "origins": ",".join(self.only_include_decisions_from),
+                },
+            )
+            resp.raise_for_status()
+        except Exception as e:
+            logger.error(f"pycrowdsec got error {e}")
+            if first_time == "true":
+                self.death_reason = e
+                raise e
+        self.process_response(resp.json())
+
+    def run(self):
+        self.cycle("true")  # So we catch errors on startup
+
+        def _thread_cycle():
+            while True:
+                sleep(self.interval)
+                self.cycle("false")
+
+        self.t = threading.Thread(target=_thread_cycle, daemon=True)
+        self.t.start()
+
+    def is_running(self):
+        return self.t.is_alive()
+
+    @abstractmethod
+    def process_response(self, response):
+        pass
+
+    @abstractmethod
+    def __post_init__(self, **kwargs):
+        pass
+
+
+class StreamClient(BaseStreamClient):
+    def __post_init__(self, **kwargs):
+        if "redis_connection" in kwargs:
+            self.cache = RedisCache(redis_connection=kwargs["redis_connection"])
+        else:
+            self.cache = Cache()
 
     def get_action_for(self, item):
         return self.cache.get(item)
 
     def get_current_decisions(self):
         return self.cache.get_all()
-
-    def _run(self):
-        session = requests.Session()
-        session.headers.update(
-            {"X-Api-Key": self.api_key, "User-Agent": self.user_agent},
-        )
-        first_time = "true"
-        while True:
-            try:
-                resp = session.get(
-                    url=f"{self.lapi_url}v1/decisions/stream",
-                    params={
-                        "startup": first_time,
-                        "scopes": ",".join(self.scopes),
-                    },
-                )
-                resp.raise_for_status()
-            except Exception as e:
-                logger.error(f"pycrowdsec got error {e}")
-                if first_time == "true":
-                    self.death_reason = e
-                    return
-                sleep(self.interval)
-                continue
-            self.process_response(resp.json())
-            first_time = "false"
-            sleep(self.interval)
 
     def process_response(self, response):
         if response["new"] is None:
@@ -120,9 +190,29 @@ class StreamClient:
         for decision in response["new"]:
             self.cache.insert(decision["value"], decision["type"])
 
-    def run(self):
-        self.t = threading.Thread(target=self._run, daemon=True)
-        self.t.start()
 
-    def is_running(self):
-        return self.t.is_alive()
+class StreamDecisionClient(BaseStreamClient):
+    def __post_init__(self, **kwargs):
+        self.deleted_decisions = queue.SimpleQueue()
+        self.new_decisions = queue.SimpleQueue()
+
+    def get_new_decision(self):
+        while not self.new_decisions.empty():
+            yield self.new_decisions.get()
+
+    def get_deleted_decision(self):
+        while not self.deleted_decisions.empty():
+            yield self.deleted_decisions.get()
+
+    def process_response(self, response):
+        if response["new"] is None:
+            response["new"] = []
+
+        if response["deleted"] is None:
+            response["deleted"] = []
+
+        for decision in response["deleted"]:
+            self.deleted_decisions.put(decision)
+
+        for decision in response["new"]:
+            self.new_decisions.put(decision)
---
+++ tests/test_mtls.py
@@ -0,0 +1,55 @@
+import pytest
+from requests.exceptions import HTTPError
+
+from pycrowdsec.client import StreamDecisionClient
+
+
+def test_tls_mutual(crowdsec, certs_dir):
+    """TLS with two-way bouncer/lapi authentication"""
+
+    lapi_env = {
+        "CACERT_FILE": "/etc/ssl/crowdsec/ca.crt",
+        "LAPI_CERT_FILE": "/etc/ssl/crowdsec/lapi.crt",
+        "LAPI_KEY_FILE": "/etc/ssl/crowdsec/lapi.key",
+        "USE_TLS": "true",
+        "LOCAL_API_URL": "https://localhost:8080",
+    }
+
+    certs = certs_dir(lapi_hostname="lapi")
+
+    volumes = {
+        certs: {"bind": "/etc/ssl/crowdsec", "mode": "ro"},
+    }
+
+    with crowdsec(environment=lapi_env, volumes=volumes) as cs:
+        cs.wait_for_log("*CrowdSec Local API listening*")
+        # TODO: wait_for_https
+        cs.wait_for_http(8080, "/health", want_status=None)
+
+        port = cs.probe.get_bound_port("8080")
+        lapi_url = f"https://localhost:{port}/"
+
+        bouncer = StreamDecisionClient(
+            "",
+            lapi_url,
+            key_path=(certs / "bouncer.key").as_posix(),
+            cert_path=(certs / "bouncer.crt").as_posix(),
+            ca_cert_path=(certs / "ca.crt").as_posix(),
+        )
+
+        bouncer.cycle("true")
+
+        bouncer = StreamDecisionClient(
+            "",
+            lapi_url,
+            key_path=(certs / "agent.key").as_posix(),
+            cert_path=(certs / "agent.crt").as_posix(),
+            ca_cert_path=(certs / "ca.crt").as_posix(),
+        )
+
+        with pytest.raises(HTTPError, match="403"):
+            bouncer.cycle("true")
+
+        cs.wait_for_log(
+            "*client certificate OU (?agent-ou?) doesn't match expected OU (?bouncer-ou?)*"
+        )
